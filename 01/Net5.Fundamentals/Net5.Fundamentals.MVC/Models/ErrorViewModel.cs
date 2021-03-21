20650af8cfd984eb41f7307d1a972df83508f8c1
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-03-20 19:55:27 -0500

    Session 01 - I

---
+++ 01/Net5.Fundamentals/Net5.Fundamentals.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net5.Fundamentals.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
