834cb420e217e545bd12ca0f7c583646510b41f9
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-03-27 21:25:39 -0500

    Session 03 y 04 update

---
+++ 03/Net5.Fundamentals/Net5.Fundamentals.ControlStructure/ControlStructureImplement.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net5.Fundamentals.ControlStructure
+{
+    public class ControlStructureImplement
+    {
+        public void IfStructure()
+        {
+            int precio = 300;
+
+            Console.WriteLine("If Structure");
+            Console.WriteLine("============");
+            Console.Write("Ingrese un precio (Entero) : ");
+
+            precio = Convert.ToInt32(Console.ReadLine());
+            if (precio > 100)
+            {
+                Console.WriteLine("El precio es mayor que 100");
+            }
+        }
+
+        public void IfElseIfStructure()
+        {
+            int precio = 300;
+
+            Console.WriteLine("If Else If Structure");
+            Console.WriteLine("====================");
+            Console.Write("Ingrese un precio (Entero) : ");
+            precio = Convert.ToInt32(Console.ReadLine());
+
+            if (precio == 100)
+            {
+                Console.WriteLine("El precio es 100");
+            }else if(precio == 200){
+                Console.WriteLine("El precio es 200");
+            }
+            else
+            {
+                Console.WriteLine("El precio no es ni 100 ni 200");
+            }
+        }
+        public void IfNested()
+        {
+            int precio = 300;
+
+            Console.WriteLine("If Nested");
+            Console.WriteLine("============");
+            Console.Write("Ingrese un precio (Entero) : ");
+            precio = Convert.ToInt32(Console.ReadLine());
+
+            if (precio >= 0)
+            {
+                if(precio > 100)
+                {
+                    Console.WriteLine("Es un precio de oferta");
+                }
+                else
+                {
+                    Console.WriteLine("Es un precio de rebaja");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El precio no puede ser negativo");
+            }
+        }
+        public void ForStructure()
+        {            
+            Console.WriteLine("For Structure");
+            Console.WriteLine("============");
+            Console.Write("Ingrese la cantidad de repeticiones (Entero) : ");
+            int limit = 0;
+            //try
+            //{
+            //    limit = int.Parse(Console.ReadLine());
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+            
+            int.TryParse(Console.ReadLine(),out limit);
+                        
+            for (int x = 1; x <= limit; x++)
+            {
+                Console.WriteLine($"x={x}");
+            }
+        }
+
+        public void WhileStructure()
+        {
+            int stock;
+            int peopleCount = 0;
+            int deliveryQuantity = 0;
+
+            Console.WriteLine("While Structure");
+            Console.WriteLine("===============");
+            Console.Write("Ingrese el stock inicial (Entero) : ");
+            stock = Convert.ToInt32(Console.ReadLine());
+
+            while (stock >=10)
+            {
+                Console.Write("Ingrese cantidad a entregar (Entero) : ");
+                deliveryQuantity = Convert.ToInt32(Console.ReadLine());
+
+                if (stock >= deliveryQuantity)
+                {
+                    stock = stock - deliveryQuantity;
+                    peopleCount++;
+                }
+            };
+
+            Console.WriteLine($"Stock final = {stock}");
+            Console.WriteLine($"Cantidad de personas atentidas = {peopleCount}");
+        }
+        public void DoWhileStructure()
+        {
+            int score = 0;
+
+            Console.WriteLine("Do While Structure");
+            Console.WriteLine("==================");
+            
+            do{
+                Console.Write("Ingrese su nota (Entero) : ");
+                score = Convert.ToInt32(Console.ReadLine());
+
+                if (score >= 11 && score <=20)
+                {
+                    Console.WriteLine("Aprobaste :)");
+                }
+                else if(score>=0 && score <=10)
+                {
+                    Console.WriteLine("Desaprobaste :(");
+                }
+                else
+                {
+                    Console.WriteLine("Es una nota invalida");
+                }
+            } while (score!=0);
+        }
+        public void SwitchStructure()
+        {
+            int numberDay;
+            string day;
+
+            Console.WriteLine("Switch Structure");
+            Console.WriteLine("================");
+            Console.Write("Ingrese dia de la semana laborable (0 - 5) : ");
+            numberDay = Convert.ToInt32(Console.ReadLine());
+
+            switch (numberDay)
+            {
+                case 1:
+                    day = "Lunes";
+                    break;
+                case 2:
+                    day = "Martes";
+                    break;
+                case 3:
+                    day = "Miercoles";
+                    break;
+                case 4:
+                    day = "Jueves";
+                    break;
+                case 5:
+                    day = "Viernes";
+                    break;
+                default:
+                    day = "No es un dia laborable";
+                    break;
+            }
+            Console.WriteLine(day);
+        }
+
+        public void BreakCommand()
+        {
+            Console.WriteLine("Break Command");
+            Console.WriteLine("==============");
+            Console.Write("Ingrese la cantidad de repeticiones (Entero) : ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            int x = 0;
+
+            for (x = 1; x <= limit; x++)
+            {
+                if (x == 4)
+                {
+                    break;
+                }
+                Console.WriteLine($"x={x}");
+            }
+        }
+
+        public void ContinueCommandInFor()
+        {
+            Console.WriteLine("Continue Command in For");
+            Console.WriteLine("=======================");
+            Console.Write("Ingrese la cantidad de repeticiones (Entero) : ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            int x = 0;
+
+            for (x = 1; x <= limit; x++)
+            {
+                if (x < 9)
+                {
+                    continue;
+                }
+                Console.WriteLine($"x={x}");
+            }
+        }
+
+        public void ContinueCommandInWhile()
+        {
+            Console.WriteLine("Continue Command in While");
+            Console.WriteLine("=========================");
+            Console.Write("Ingrese la cantidad de repeticiones (Entero) : ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            int x = 0;
+
+            while(x < limit)
+            {
+                x++;
+                if (x < 5)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"x={x}");
+            }
+        }
+        public void GoToCommandInSwitch()
+        {
+            int cost = 0;
+            Console.WriteLine("GoTo Command in Switch");
+            Console.WriteLine("======================");
+            Console.WriteLine("Tamaños de cafe : ");
+            Console.WriteLine("1 = Regular");
+            Console.WriteLine("2 = Grande");
+            Console.WriteLine("3 = Venti");
+            Console.Write("Por favor ingrese su eleccion : ");
+            int opt = Convert.ToInt32(Console.ReadLine());
+
+            switch (opt)
+            {
+                case 1:
+                    //cost = costo + 25;
+                    cost += 25;
+                    break;
+                case 2:                    
+                    cost += 25;
+                    goto case 1;                    
+                case 3:                    
+                    cost += 50;
+                    goto case 1;                    
+                default:
+                    Console.WriteLine("Selección inválida");
+                    break;
+            }
+
+            if(cost != 0)
+            {
+                Console.WriteLine($"Por favor inserte {cost} soles");
+            }
+
+            Console.WriteLine("Gracias por su compra.");
+        }
+        public void ForEachStructure()
+        {
+            Console.WriteLine("For Each Structure");
+            Console.WriteLine("==================");
+            Console.Write("Ingrese la cantidad de elementos (Entero) : ");
+            
+            int peopleCount = Convert.ToInt32(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < peopleCount; i++)
+            {
+                people.Add(new Person
+                {
+                    Nombre = $"First Name- {i}",
+                    ApellidoPaterno = $"Last Name- {i}",
+                    ApellidoMaterno = $"Sur Name- {i}",
+                });
+            }
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"Complete Name : {person.Nombre} {person.ApellidoPaterno} {person.ApellidoMaterno}");
+            }
+        }
+        public void TryCatchStructure()
+        {
+            int dividendo = 100;
+            int divisor = 0;
+            int resultado = 0;
+
+            try
+            {
+                Console.WriteLine("Try Catch Structure");
+                Console.WriteLine("===================");
+                Console.Write("Ingrese Dividendo (Entero) : ");
+                dividendo = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese Divisor (Entero) : ");
+                divisor = Convert.ToInt32(Console.ReadLine());
+
+                resultado = dividendo / divisor;
+
+                Console.WriteLine($"{dividendo} / {divisor} = {resultado}");
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+            }
+        }
+        public void TryCatchFinallyStructure()
+        {
+            int dividendo = 100;
+            int divisor = 0;
+            int resultado = 0;
+            bool succeedOperation = false;
+
+            try
+            {
+                Console.WriteLine("Try Catch Structure");
+                Console.WriteLine("===================");
+                Console.Write("Ingrese Dividendo (Entero) : ");
+                dividendo = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese Divisor (Entero) : ");
+                divisor = Convert.ToInt32(Console.ReadLine());
+
+                resultado = dividendo / divisor;
+                Console.WriteLine($"{dividendo} / {divisor} = {resultado}");
+                succeedOperation = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+                succeedOperation = false;
+            }
+            finally
+            {
+                if (succeedOperation)
+                {
+                    Console.WriteLine($"La operación fue exitosa :)");
+                }
+                else
+                {
+                    Console.WriteLine($"La operación no fue exitosa :(");
+                }
+            }
+        }
+    }
+}
---
+++ 04/Net5.Fundamentals/Net5.Fundamentals.ListAndThread/ListAndThread.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net5.Fundamentals.ListAndThread
+{
+    public class ListAndThread
+    {
+        public void ArraySample()
+        {
+            Console.WriteLine("Array");
+            Console.WriteLine("=====");
+
+            int[] array = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = i;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"array[{i}] : {array[i]}");
+            }
+            Console.WriteLine($"Size : {array.Length}");
+            
+            Console.WriteLine($"Inicializacion y asignacion");
+            int[] array2 = new int[] { 1, 2, 3, 4, 5 };
+            int[] array3 = { 6, 7, 8, 9, 10 };
+            
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"array3[{i}] : {array3[i]}");
+            }
+
+            Console.WriteLine($"Arreglo multi-dimensional");
+            int[,] multidimensionalArray = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine($"multidimensionalArray[{i},{j}] : {multidimensionalArray[i, j]}");
+                }
+            }
+
+            int[][] matrix = new int[6][];
+            matrix[0] = new int[] { 1, 2, 3, 4, 5 };
+            matrix[1] = new int[] { 1, 2, 3, 4, 5, 6 };
+            matrix[2] = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            //matrix[3]
+            //matrix[4]
+            //matrix[5]
+
+            for (int i = 0; i < 6; i++)
+            {
+                if (matrix[i] == null) continue;
+
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.WriteLine($"matrix[{i}],[{j}] : {matrix[i][j]}");
+                }
+            }
+
+            Console.WriteLine("String Array");
+            string[] weekDays = new string[] {"Lunes","Martes","Miercoles","Jueves","Viernes" };
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                Console.WriteLine(weekDays[i]);
+            }
+        }
+
+        public void ListSample()
+        {
+            Console.WriteLine("List");
+            Console.WriteLine("====");
+
+            List<int> numberList = new List<int>();
+            numberList.Add(1);
+            numberList.Add(3);
+            numberList.Add(5);
+            numberList.Add(7);
+                        
+            numberList.ForEach(num =>
+            {
+                Console.WriteLine($"Numbers : {num}");
+            });
+
+            List<string> cities = new List<string>()
+            {
+                "Lima",
+                "Villa el Salvador",
+                "Rimac",
+                "Callao",
+                "Jesus Maria",
+                "San Juan De Lurigancho",
+                "Miraflores",
+                "San Isidro",
+                "Villa Maria del Triunfo"
+            };
+
+            cities.ForEach(city =>
+            {
+                Console.WriteLine($"Cities : {city}");
+            });
+
+            Console.WriteLine("Ciudades de tamaño menor o igual a 6");
+            cities.Where(city => city.Length <= 6).ToList().ForEach(c =>
+            {
+                Console.WriteLine($"Cities : {c}");
+            });
+
+            List<Person> people = new List<Person>
+            {
+                new Person{Name = "Erick",Age=38,Sex="M"},
+                new Person{Name = "Jorge",Age=30,Sex="M"},
+                new Person{Name = "Azucena",Age=22,Sex="F"},
+                new Person{Name = "Jeniffer",Age=21,Sex="F"}
+            };
+
+            Console.WriteLine("Mujeres del salon de clase");
+            people.Where(person => person.Sex == "F").ToList().ForEach(p =>Console.WriteLine(p.Name));
+            
+            Console.WriteLine($"La suma de las edades es : {people.Sum(p=>p.Age)}");
+        }
+
+        public void DictionarySample()
+        {
+            Console.WriteLine("Dictionary");
+            Console.WriteLine("==========");
+
+            Dictionary<string, Person> people = new Dictionary<string, Person>();
+
+            people.Add("12345678", new Person { DNI= "12345678", Name = "Erick", Age = 38, Sex = "M" });
+            people.Add("56428632", new Person { DNI= "56428632", Name = "Jorge", Age = 30, Sex = "M" });
+            people.Add("41635163", new Person { DNI= "41635163", Name = "Azucena", Age = 22, Sex = "F" });
+            people.Add("88382238", new Person { DNI= "88382238", Name = "Jeniffer", Age = 21, Sex = "F" });
+
+            Console.WriteLine($"Azucena : {people.Where(kvp => kvp.Value.Name == "Azucena").Select(s => s.Key).First()} ");
+            Console.WriteLine($"Key : 56428632, value {people["56428632"].Name}");
+        }
+    }
+}
